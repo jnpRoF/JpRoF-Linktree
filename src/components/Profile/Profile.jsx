4ec6e5968f670dfde765db524947ae4ddd115438
Johnson <ogbonnayajprof2015@gmail.com>
2022-10-26 15:07:44 +0100

    feat:develop profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,17 @@
 const Profile = () => {
-    return <div>Profile</div>
-}
+  return (
+    <div class="profile_cont">
+      <div class="profile-wrap">
+        <img
+          src="https://avatars.githubusercontent.com/u/97938411?v=4"
+          alt=""
+          id="profile__img"
+        />
+      </div>
+      <h3 id="twitter">OgbonnayaJohns3</h3>
+      {/* <h3 id="slack">OgbonnayaJohns3</h3> */}
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
