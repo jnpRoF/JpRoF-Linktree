3fc68a8a2250399976e3d72a1fd6a54679ea2d02
Johnson <ogbonnayajprof2015@gmail.com>
2022-10-28 13:41:28 +0100

    style:  fix profile-image style

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,18 @@
+import webDefault from "../../assets/webDefault.png";
+import "./Profile.css";
 const Profile = () => {
   return (
-    <div class="profile_cont">
-      <div class="profile-wrap">
+    <div className="profile_cont">
+      <div className="profile_wrap">
         <img
           src="https://avatars.githubusercontent.com/u/97938411?v=4"
+          // src={webDefault}
           alt=""
           id="profile__img"
         />
+        <h2 id="twitter">OgbonnayaJohns3</h2>
+        {/* <h3 id="slack">OgbonnayaJohns3</h3> */}
       </div>
-      <h3 id="twitter">OgbonnayaJohns3</h3>
-      {/* <h3 id="slack">OgbonnayaJohns3</h3> */}
     </div>
   );
 };
