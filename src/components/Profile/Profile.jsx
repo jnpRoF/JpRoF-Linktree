313e79a20f4b656df64316fb9cd08b59e336bbb1
Johnson <ogbonnayajprof2015@gmail.com>
2022-10-31 16:39:02 +0100

    style: fix hover

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,4 @@
+import camera from "../../assets/camera-01.png";
 import Icon from "../../assets/Icon.png";
 import "./Profile.css";
 const Profile = () => {
@@ -5,12 +6,14 @@ const Profile = () => {
     <div className="profile_cont">
       <div className="profile_wrap">
         <div className="img_wrap">
-          <div className="profile_img_cont"></div>
+          {/* <div className="profile_img_cont"> */}
           <img
             src="https://avatars.githubusercontent.com/u/97938411?v=4"
             alt=""
             id="profile__img"
           />
+          {/* </div> */}
+          <img src={camera} alt="" className="cam" />
         </div>
         <h2 id="twitter">OgbonnayaJohns3</h2>
         {/* <h3 id="slack">OgbonnayaJohns3</h3> */}
