1fba3a4d17683777f9e6270e7568d431ecc4f3bf
Johnson <ogbonnayajprof2015@gmail.com>
2022-10-31 11:50:30 +0100

    chore: create data json

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,20 @@
 import webDefault from "../../assets/webDefault.png";
+import camera from "../../assets/camera-01.png";
 import "./Profile.css";
 const Profile = () => {
   return (
     <div className="profile_cont">
       <div className="profile_wrap">
-        <img
-          src="https://avatars.githubusercontent.com/u/97938411?v=4"
-          // src={webDefault}
-          alt=""
-          id="profile__img"
-        />
+        <div className="img_wrap">
+          <img
+            // src="https://avatars.githubusercontent.com/u/97938411?v=4"
+            src={webDefault}
+            alt=""
+            id="profile__img"
+            // onHover={}
+          />
+          <img src={camera} alt="" className="cam" />
+        </div>
         <h2 id="twitter">OgbonnayaJohns3</h2>
         {/* <h3 id="slack">OgbonnayaJohns3</h3> */}
       </div>
---
+++ src/constants/data.js
@@ -0,0 +1,19 @@
+const data = [
+  { id: "btn__zuri", uri: "https://training.zuri.team/" },
+  {
+    id: "books",
+    uri: "https://books.zuri.team/python-for-beginners?ref_id=JpRoF",
+  },
+  {
+    id: "book__python",
+    uri: "https://background.zuri.team",
+  },
+  {
+    id: "pitch",
+    uri: "https://training.zuri.team/",
+  },
+  {
+    id: "book__design",
+    uri: "https://books.zuri.team/design-rules",
+  },
+];
