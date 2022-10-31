9924af6c29e11f805d56dfdf0858a20ac593e426
Johnson <ogbonnayajprof2015@gmail.com>
2022-10-31 13:28:58 +0100

    chore: develop footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,15 @@
+import I4G from "../../asset/I4G.png";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <p>
+        Zuri <span>.</span> Internship
+      </p>
+      <p>HNG Internship 9 Frontend Task</p>
+      <img src={I4G} />
+    </div>
+  );
 };
 
 export default Footer;
